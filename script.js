aedc33c76803f09146ae4d3c9f7a3fcc3f9f2baf
Ata <ririn110283@gmail.com>
2024-10-28 20:26:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function buka() {
 function tutup() {
     document.getElementById("info").style.display="none";
 }
+function side() {
+    document.querySelector('.aside').style.visibility= "visible";
+    document.querySelector
+}
+function cside() {
+    document.querySelector('.aside').style.visibility= "hidden";
+}
+window.addEventListener('scroll', function() {
+    window.scrollTo(0, window.scrollY);
+});
